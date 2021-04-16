e256c974894ab815fb0eeb2c18e5973e4e179f97
mrbarch <sasha.barchuk.99@gmail.com>
2021-04-16 12:47:31 +0300

    vue1

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="todo-item">
+    <div class="todo-item-left">
+      <input type="checkbox" v-model="todo.completed">
+      <div v-if="!todo.editing" @dblclick="editTodo(todo)" class="todo-item-label"
+           :class="{ completed : todo.completed }">
+        {{ todo.title }}
+      </div>
+      <input v-else class="todo-item-edit" type="text" v-model="todo.title" @blur="doneEdit(todo)"
+             @keyup.enter="doneEdit(todo)" @keyup.esc="cancelEdit(todo)" v-focus>
+    </div>
+    <div class="remove-item" @click="removeTodo(index)">
+      &times;
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'todo-item',
+}
+</script>
